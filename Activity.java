55be175ab667634bfc8eaf37fd08fdfbd2201efa
emmassheppardPNNL <emma.sheppard@pnnl.gov>
2024-10-10 10:52:05 -0700

    cat's name

--- Activity.java
+++ Activity.java
@@ -3,6 +3,7 @@ public class Activity {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
+        System.out.println("my cat's name is name");
 
     }
 
